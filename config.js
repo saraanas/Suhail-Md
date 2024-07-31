38031b9b1a6355df844da164818fc095541b8d8f
jamilaanas <maryamstatut@gmail.com>
2024-07-31 15:12:18 +0100

    config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ module.exports = {
   KOYEB_API : process.env.KOYEB_API  || "false",
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "CTWvDfFghe85UerGcnGuhhKe",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-None-XgEFkIb7YLXmDVUrlqgYT3BlbkFJszbvHpuowXKX0TS4F851",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
